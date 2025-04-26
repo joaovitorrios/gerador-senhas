8876f5d24db08886c77bd97038d3b202d4ab8c11
João Vitor <joaovitorriosdev@gmail.com>
2025-04-26 20:52:22 -0300

    codigo css e javascript

---
+++ sscript.js
@@ -0,0 +1,29 @@
+const passwordInput = document.getElementById('password');
+const lengthInput = document.getElementById('length');
+const generateBtn = document.getElementById('generate');
+const copyBtn = document.getElementById('copy');
+
+// Caracteres possíveis
+const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+=-[]{}|;:,.<>?/";
+
+generateBtn.addEventListener('click', () => {
+    const length = lengthInput.value;
+    let password = "";
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        password += characters[randomIndex];
+    }
+
+    passwordInput.value = password;
+});
+
+copyBtn.addEventListener('click', () => {
+    if (passwordInput.value) {
+        passwordInput.select();
+        document.execCommand('copy');
+        alert('Senha copiada!');
+    } else {
+        alert('Nenhuma senha para copiar!');
+    }
+});
